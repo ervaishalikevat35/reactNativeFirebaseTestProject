971f97f6966e34ab60b5e0ffea3a67d4a73a630c
vaishalikevat <vaishali@softopstechnologies.com>
2022-08-04 16:59:39 +0530

    rssolve issue

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet } from 'react-native';
 import Routes from './src/Navigation/Routes';
 import TestHooks from './src/screens/Test/TestHooks';
 import React, { useEffect, useState } from 'react'
-// import { firebase } from './src/screens/Home/firbase'
 
 const MyComponent = () => {
 
